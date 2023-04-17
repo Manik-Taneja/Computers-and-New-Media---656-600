6acc075c3b57b5d43efa7f9199ad0a75f783c241
Manik Taneja <maniktaneja712@gmail.com>
2023-04-16 23:07:54 -0500

    [Manik]: Added Main.py for flask

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from Backend.processing import *
+app = Flask(__name__)
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    return render_template("LandingPage.html")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("LandingPage.html")
+
+@app.route("/Summarizer", methods=["GET", "POST"])
+def Summarizer():
+    return render_template("Summarizer.html")
+
+@app.route("/TeamMembers", methods=["GET", "POST"])
+def TeamMembers():
+    return render_template("TeamMembers.html")
+
+
+@app.route("/handle_Summarizer", methods=["GET", "POST"])
+def handle_Summarizer():
+    if request.method == "POST" and "Link" in request.form:
+        YTLink = request.form["Link"]
+        return get_link(YTLink)
+
+
+app.run(port=8000, debug=True)
